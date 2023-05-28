bc9edf5ec7792d6bfad33a8dbc8ec0d2567f88b7
Kishan Chandravadiya <123851397+Kishan-Ahir@users.noreply.github.com>
2023-05-28 20:01:22 +0530

    Update app.js

--- clean code by making local module/app.js
+++ clean code by making local module/app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const local_module = require('./local module');
-//here in localmodule variable we are storing one function which we are exporting frpm local module file. so here node we look into local module file and see what is mentioned in module.export that will get export from local module file.
 
-const server = http.createServer(local_module);//here node js will execute this function when server will get incoming request so just mention function name(local_module) don't execute it.(local_module())
+
+const server = http.createServer(local_module);
 
 server.listen(4000);
